78180826008bcc4a82b6239ed8715c78f3a591dc
Asif Jardary <99320140+AsifJardary@users.noreply.github.com>
2023-07-03 13:51:24 +0600

    Add files via upload

---
+++ A - Make Even/A - Make Even/A - Make Even.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int i = 34683;
+	
+	string str = to_string(i);
+	cout << str;
+	int z = 0;
+	cout << "\n";
+	int str_length = str.length();tr4
+	int first = str[str_length - 1];
+	if()
+	while (str[z] != NULL)
+	{
+		int p = str[z] - '0';
+		if()
+		if (p % 2 == 0 || p == 0)
+		{
+			cout << p << "\n";
+		}
+		z++;
+	}
+
+}
