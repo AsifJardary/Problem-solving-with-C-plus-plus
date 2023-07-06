7ff8854813e4ba369005f598ba9d5baa34a32a7f
Asif Jardary <99320140+AsifJardary@users.noreply.github.com>
2023-07-06 13:57:40 +0600

    Add files via upload

---
+++ B. Make it Divisible by 25.cpp
@@ -0,0 +1,15 @@
+    #include <iostream>
+    #include<string>
+    using namespace std;
+    int main()
+    {
+        string atr=to_string();
+     int i=11;
+     float f=12.3;
+     /*
+    string str= to_string(i);
+    string str1= to_string(f);
+    cout<<"string value of integer i is :"<<str<<"\n";
+    cout<<"string value of f is : "<< str1;
+    */
+    }
