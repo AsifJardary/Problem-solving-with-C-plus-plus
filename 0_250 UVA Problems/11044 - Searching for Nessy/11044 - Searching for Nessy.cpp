3d8791a62788fa07c2e487c1fcc7d78c380bed09
Asif Jardary <99320140+AsifJardary@users.noreply.github.com>
2023-07-27 14:01:53 +0600

    Add files via upload

---
+++ 0_250 UVA Problems/11044 - Searching for Nessy/11044 - Searching for Nessy.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+
+{
+
+    int n,i,a,b;
+
+    cin>>n;
+
+    for(i=0;i<n;i++)
+
+    {
+
+        cin>>a>>b;
+
+    cout<<( (a/3) * (b/3) )<<endl;
+
+
+}
+
+
+
+    return 0;
+
+}
